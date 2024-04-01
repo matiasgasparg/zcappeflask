d3a26dbc3ad8e85529c8612e89d40c5358712490
matiasgasparg <matiasgasparg@gmail.com>
2024-04-01 17:30:55 -0300

    cambio de servidor, pythonanywhere

--- run.py
+++ run.py
@@ -1,8 +1,5 @@
 from app import init_app
 
 if __name__ == "__main__":
-    # Inicializar la aplicación Flask
     app = init_app()
-    
-    # Ejecutar la aplicación Flask en todas las direcciones IP en el puerto 5000
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)  # Ejecutar la aplicación en modo debug
